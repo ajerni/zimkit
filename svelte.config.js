54774c6a4024280f2d66a2aeef9a61a16f52c0db
ajerni <ajerni@gmail.com>
2021-09-24 14:00:37 +0200

    again

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,22 @@
 //import adapter from '@sveltejs/adapter-static';
-		// adapter: adapter({
-		//	pages: 'build',
-		//	assets: 'build',
-		//	fallback: null
-	
+// adapter: adapter({
+//	pages: 'build',
+//	assets: 'build',
+//	fallback: null
+
 import adapter from '@sveltejs/adapter-netlify';
- 
+
 const config = {
-  kit: {
-    adapter: adapter(),
-    // hydrate the <div id="svelte"> element in src/app.html
-    target: '#svelte'
+	kit: {
+		adapter: adapter(),
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
 	},
 	vite: {
 		define: {
 			'process.env': process.env
 		}
-	},
+	}
 };
- 
-export default config
+
+export default config;

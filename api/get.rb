beea766937cc2a63efb3f4f43e122dbad9bd81b2
ajerni <ajerni@gmail.com>
2021-09-24 16:56:55 +0200

    httpx ruby

--- api/get.rb
+++ api/get.rb
@@ -1,5 +1,4 @@
-require 'httparty'
+require "httpx"
 
-response = HTTParty.get('https://api.particle.io/v1/devices/e00fce6839e00714b083442d/temperature?access_token=906d5e4a9041e4c0773cad80ccf23490fe83e76c')
-puts response.body if response.code == 200
-p response
+response = HTTPX.get("https://api.particle.io/v1/devices/e00fce6839e00714b083442d/temperature?access_token=906d5e4a9041e4c0773cad80ccf23490fe83e76c")
+puts response.body if response.status == 200

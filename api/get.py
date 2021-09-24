e94617aba5a3101befd4451b9556587afc3ac09a
ajerni <ajerni@gmail.com>
2021-09-24 16:41:14 +0200

    ruby

--- api/get.py
+++
@@ -1,7 +0,0 @@
-# https://api.particle.io/v1/devices/' + myDevice + '/' + variable + '?access_token=' + myApiKey
-
-import requests.get
-# ploads = {'things':2,'total':25}
-r = requests.get('https://api.particle.io/v1/devices/e00fce6839e00714b083442d/temperature?access_token=906d5e4a9041e4c0773cad80ccf23490fe83e76c')
-print(r.text)
-print(r.url)
---
+++ api/get.rb
@@ -0,0 +1,4 @@
+require 'httparty'
+
+response = HTTParty.get('https://api.particle.io/v1/devices/e00fce6839e00714b083442d/temperature?access_token=906d5e4a9041e4c0773cad80ccf23490fe83e76c')
+puts response.body if response.code == 200

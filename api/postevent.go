9ff072f6e129146c97ce7b2e80afc09df343bd10
ajerni <ajerni@gmail.com>
2021-09-25 11:46:59 +0200

    net url

--- api/postevent.go
+++ api/postevent.go
@@ -15,6 +15,7 @@ import (
    "io/ioutil"
    "log"
    "net/http"
+   "net/url"
    "fmt"
 )
 
@@ -26,14 +27,12 @@ func Handler(w http.ResponseWriter, r *http.Request) {
    keys := r.URL.Query()["key"]
    key := keys[0]
 
-   url := "https://api.particle.io/v1/devices/events"
-
    params := url.Values{}
   	params.Add("name", key)
   	params.Add("access_token", "906d5e4a9041e4c0773cad80ccf23490fe83e76c")
   	params.Add("data", "")
   
-   resp, err := http.PostForm(url, params) 
+   resp, err := http.PostForm("https://api.particle.io/v1/devices/events", params) 
   
    defer resp.Body.Close()
    body, err := ioutil.ReadAll(resp.Body)

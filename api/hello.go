f2d50dddba43f8dfb310d3347154ebf604618774
ajerni <ajerni@gmail.com>
2021-09-25 15:19:24 +0200

    hello

---
+++ api/hello.go
@@ -0,0 +1,15 @@
+//https://zimkit.vercel.app/api/hello?name=name
+
+package handler
+
+import (
+   "fmt"
+)
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+
+   names := r.URL.Query()["name"]
+   name := names[0]
+
+   fmt.Fprintf(w, "Hello " + name)
+}

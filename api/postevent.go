57f74c240fa18d1c5e343a0095a2fc2327b6200b
ajerni <ajerni@gmail.com>
2021-09-25 12:03:15 +0200

    doneeee

--- api/postevent.go
+++ api/postevent.go
@@ -1,14 +1,4 @@
-// function publishEvent(){
-// 	let eventname = "alarmall";
-// 	let daten = "";
-// 	fetch("https://api.particle.io/v1/devices/events", {
-// 		body: "name=" + eventname + "&data=" + daten + "&access_token=" + AUTH_TOKEN,
-// 		headers: {"Content-Type": "application/x-www-form-urlencoded"},
-// 		method: "POST"
-// 	})
-// }
-
-//GO
+//POST Request to Particle cloud publishing an event to all listeners
 package handler
 
 import (
@@ -19,18 +9,17 @@ import (
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 
-	//fmt.Fprintf(w, "reached at least now")
-   //https://zimkit.vercel.app/api/get?key=nameOfEventToPublish alarmall (Parameter key auslesen) 
+   //use like this: https://zimkit.vercel.app/api/get?key=nameOfEventToPublish z.B alarmall
 
-   keys := r.URL.Query()["key"]
+   keys := r.URL.Query()["key"] //hier wird der parameter key ausgelesen (eventname der published werden soll)
    key := keys[0]
 
-   params := url.Values{}
+   params := url.Values{} //definiert den Body des POST request
   	params.Add("name", key)
   	params.Add("access_token", "906d5e4a9041e4c0773cad80ccf23490fe83e76c")
   	params.Add("data", "")
   
-   resp, err := http.PostForm("https://api.particle.io/v1/devices/events", params) 
+   resp, err := http.PostForm("https://api.particle.io/v1/devices/events", params) //Enthält automatisch den richtigen Header mit Content Type
 
    if err != nil {   
       log.Printf("Request Failed: %s", err)

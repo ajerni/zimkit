83ba430313c4b1297c0befc588f9ce5d2ac01137
ajerni <ajerni@gmail.com>
2021-09-30 13:05:14 +0200

    FormValue

--- api/form.go
+++ api/form.go
@@ -11,12 +11,13 @@ func PostMyFormHandler(w http.ResponseWriter, r *http.Request) {
    //use like this: https://zimkit.vercel.app/api/form (als action link im form)
 
    //f := r.URL.Query().Get("f") // um Query Vars auszulesen
-   start := r.Form.Get("start")
-   end := r.Form.Get("end")
+   start := r.FormValue("start")
+   end := r.FormValue("end")
 
    w.Write([]byte(fmt.Sprintf("The start date is %s and the end date is %s", start, end)))
-   fmt.Fprintf(w, start)
+   fmt.Fprintf(w, r.FormValue("start"))
    w.Write([]byte("Ende: " + end))
+   
 
    
 }

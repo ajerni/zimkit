dc95fed20156f8a2f95aeddf9adfac022454da7e
ajerni <ajerni@gmail.com>
2021-10-01 16:13:14 +0200

    redirect only

--- api/form-redirect.go
+++ api/form-redirect.go
@@ -2,40 +2,40 @@
 package handler
 
 import (
-	"encoding/json"
-	"log"
+	// "encoding/json"
+	// "log"
 	"net/http"
 )
 
-type jsonResponse struct {
-	OK        bool   `json:"ok"`
-	Message   string `json:"message"`
-	Start     string `json:"start"`
-	End       string `json:"end"`
-	Sonstiges string `json:"sonstiges"`
-}
+// type jsonResponse struct {
+// 	OK        bool   `json:"ok"`
+// 	Message   string `json:"message"`
+// 	Start     string `json:"start"`
+// 	End       string `json:"end"`
+// 	Sonstiges string `json:"sonstiges"`
+// }
 
 func PostMyFormHandlerJson(w http.ResponseWriter, r *http.Request) {
 	
 
 	//create and send JSON response
-	resp := jsonResponse{
-		OK:        true,
-		Message:   "Hallo aus der Serverless go function",
-		Start:     "fake",
-		End:       "data",
-		Sonstiges: "Eigenes Zeugs in JSON.",
-	}
+	// resp := jsonResponse{
+	// 	OK:        true,
+	// 	Message:   "Hallo aus der Serverless go function",
+	// 	Start:     "fake",
+	// 	End:       "data",
+	// 	Sonstiges: "Eigenes Zeugs in JSON.",
+	// }
 
-	out, err := json.MarshalIndent(resp, "", "   ")
-	if err != nil {
-		log.Println(err)
-	}
+	// out, err := json.MarshalIndent(resp, "", "   ")
+	// if err != nil {
+	// 	log.Println(err)
+	// }
 
-	log.Println((string(out)))
+	// log.Println((string(out)))
 
-	w.Header().Set("Content-Type", "aplication/json")
-	w.Write(out)
+	// w.Header().Set("Content-Type", "aplication/json")
+	// w.Write(out)
 
 	http.Redirect(w, r, "https://zimkit.vercel.app/redirected", http.StatusSeeOther)
 }

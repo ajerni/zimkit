592c33199125ff7fb4fc42211a32ee6d3450b5d6
ajerni <ajerni@gmail.com>
2021-10-02 09:47:19 +0200

    redir try again

---
+++ api/redirect.go
@@ -0,0 +1,10 @@
+// Redirect
+package handler
+
+import (
+	"net/http"
+)
+
+func RedirectHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "https://iot.andierni.ch", http.StatusFound)
+}

f80baa5f4dc330f4ee552b0fb1e10b63bc8b3f18
ajerni <ajerni@gmail.com>
2021-10-03 18:26:10 +0200

    jjj

--- api/dbtest.go
+++ api/dbtest.go
@@ -2,54 +2,52 @@ package handler
 
 import (
 	"database/sql"
-    "fmt"
-	"net/http"
+	"fmt"
 	"log"
-    _ "github.com/go-sql-driver/mysql"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 func MyDbHandler(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("mysql", "andiroot:letmein@tcp(xnmxviali589.db.solnet.ch:3306)/andigodb")
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		log.Print(err.Error())
+	}
+	defer db.Close()
 
-	db, err := sql.Open("mysql", "andiroot:letmein@tcp(xnmxviali589.db.solnet.ch)/andigodb")
-
-    // if there is an error opening the connection, handle it
-    if err != nil {
-        log.Print(err.Error())
-    }
-    defer db.Close()
-
-    // Execute the query
-    results, err := db.Query("SELECT * FROM gotable WHERE id = 2")
-    if err != nil {
-        panic(err.Error()) // proper error handling instead of panic in your app
-    }
+	// Execute the query
+	results, err := db.Query("SELECT * FROM gotable WHERE id = 2")
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
 
 	fmt.Fprintf(w, fmt.Sprintf("Result: %s", results))
-   
-// // Capture connection properties.
-// cfg := mysql.Config{
-// 	// User:   os.Getenv("DBUSER"),
-// 	// Passwd: os.Getenv("DBPASS"),
-// 	User: "andiroot",
-// 	Passwd: "LetMeIn1976",
-// 	Net:    "tcp",
-// 	Addr:   "xnmxviali589.db.solnet.ch",
-// 	DBName: "andigodb",
-// }
-// // Get a database handle.
-// var err error
-// db, err = sql.Open("mysql", cfg.FormatDSN())
-// if err != nil {
-// 	log.Fatal(err)
-// }
 
-// pingErr := db.Ping()
-// if pingErr != nil {
-// 	log.Fatal(pingErr)
-// }
-// fmt.Println("Connected!")
+	// // Capture connection properties.
+	// cfg := mysql.Config{
+	// 	// User:   os.Getenv("DBUSER"),
+	// 	// Passwd: os.Getenv("DBPASS"),
+	// 	User: "andiroot",
+	// 	Passwd: "LetMeIn1976",
+	// 	Net:    "tcp",
+	// 	Addr:   "xnmxviali589.db.solnet.ch",
+	// 	DBName: "andigodb",
+	// }
+	// // Get a database handle.
+	// var err error
+	// db, err = sql.Open("mysql", cfg.FormatDSN())
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-// row := db.QueryRow("SELECT * FROM gotable WHERE id = 2")
-// fmt.Fprintf(w, fmt.Sprintf("Result %s", row))
+	// pingErr := db.Ping()
+	// if pingErr != nil {
+	// 	log.Fatal(pingErr)
+	// }
+	// fmt.Println("Connected!")
 
+	// row := db.QueryRow("SELECT * FROM gotable WHERE id = 2")
+	// fmt.Fprintf(w, fmt.Sprintf("Result %s", row))
 }

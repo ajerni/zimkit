716301c39b8f0dd1b955564e401db8ff0a6a04dd
ajerni <ajerni@gmail.com>
2021-10-05 00:00:19 +0200

    rel external

--- src/routes/heroku.svelte
+++ src/routes/heroku.svelte
@@ -2,10 +2,10 @@
 	<div class="row">
 		<h1 class="mt-3">Heroku DB</h1>
 		<div class="col-2">
-			<button><a href="https://zimkit.vercel.app/api/dbadd">Add</a></button>
+			<button><a rel="external" href="https://zimkit.vercel.app/api/dbadd">Add</a></button>
 		</div>
 		<div class="col-2">
-			<button><a href="https://zimkit.vercel.app/api/dbremove">Remove</a></button>
+			<button><a rel="external" href="https://zimkit.vercel.app/api/dbremove">Remove</a></button>
 		</div>
 		<div class="row">
 			<p />

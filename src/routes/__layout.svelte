db834f3cd4f740349718dcca6c8bc400b0b5f0b8
ajerni <ajerni@gmail.com>
2021-10-15 22:43:07 +0200

    sveltestrap

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,45 +1,61 @@
-<!-- src/routes/__layout.svelte wird für alle pages angewandt -->
-<!-- https://rodneylab.com/getting-started-with-sveltekit/ -->
+<!-- Bootstrap import -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
-<script
-	src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js"
-	integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ"
-	crossorigin="anonymous">
-</script>
+<!-- src/routes/__layout.svelte wird für alle pages angewandt -->
 
 <title>Andi's Toolkit</title>
 
-    <div class="bg-dark text-secondary px-4 py-5 text-center">
-      <div class="py-5">
-        <h1 class="display-5 fw-bold text-white">Andi's Web-Kit</h1>
-        <div class="col-lg-6 mx-auto">
-          <p class="fs-5 mb-4 text-white">
-            Svelte Frontend - Go backend
-          </p>
-		  <p class="fs-5 mb-4">
-            Reference. Template. Toolkit. Playground.
-          </p>
-          <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+<div class="bg-dark text-secondary px-4 py-5 text-center">
+	<div class="py-5">
+		<h1 class="display-5 fw-bold text-white">Andi's Web-Kit</h1>
+		<div class="col-lg-6 mx-auto">
+			<p class="fs-5 mb-4 text-white">Svelte Frontend - Go backend</p>
+			<p class="fs-5 mb-4">Reference. Template. Toolkit. Playground.</p>
+			<div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+				<a href="/"
+					><button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold custom"
+						>Home</button
+					></a
+				>
+				<a href="/zim"
+					><button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold custom"
+						>ZIM</button
+					></a
+				>
+				<a href="/error"
+					><button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold custom"
+						>Error</button
+					></a
+				>
+				<a href="/temperatur"
+					><button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold custom"
+						>Sensor</button
+					></a
+				>
+				<a href="/form"
+					><button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold custom"
+						>Form</button
+					></a
+				>
+				<a href="/heroku"
+					><button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold custom"
+						>Database</button
+					></a
+				>
+				<a href="/ajax"
+					><button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold custom"
+						>AJAX</button
+					></a
+				>
 
-
-            <a href="/"><button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Home</button></a>
-			<a href="/zim"><button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">ZIM</button></a>
-            <a href="/error"><button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Error</button></a>
-			<a href="/temperatur"><button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Sensor</button></a>
-			<a href="/form"><button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Form</button></a>
-			<a href="/heroku"><button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">Database</button></a>
-			<a href="/ajax"><button type="button" class="btn btn-outline-info btn-lg px-4 me-sm-3 fw-bold">AJAX</button></a>
-	
-			
-			<a href="https://github.com/ajerni/zimkit/"
-              ><button type="button" class="btn btn-outline-light btn-lg px-4">
-                Code
-              </button></a
-            >
-          </div>
-        </div>
-      </div>
-    </div>
+				<a href="https://github.com/ajerni/zimkit/"
+					><button type="button" class="btn btn-outline-light btn-lg px-4 custom"> Code </button></a
+				>
+			</div>
+			<button class="custom-class">gugus</button>
+		</div>
+	</div>
+</div>
 
 <main>
 	<div class="min-vh-100 w-100 p-4">
@@ -48,16 +64,36 @@
 </main>
 
 <footer>
-	<div class="bg-dark text-secondary py-4 text-center">  
-		A project by <a href="https://www.andierni.ch/" target="_blank" rel="nofollow noopener noreferrer">ERNILABS</a>.
+	<div class="bg-dark text-secondary py-4 text-center">
+		A project by <a
+			href="https://www.andierni.ch/"
+			target="_blank"
+			rel="nofollow noopener noreferrer">ERNILABS</a
+		>.
 	</div>
 </footer>
 
 <svelte:head>
-	<link
-		href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css"
-		rel="stylesheet"
-		integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU"
-		crossorigin="anonymous"
-	/>
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
 </svelte:head>
+
+<style>
+	.custom {
+		width: 8rem !important;
+	}
+
+	.custom-class {
+		display: none;
+	}
+
+	@media only screen and (max-width: 576px) {
+		.custom-class {
+			display: block;
+			marting: auto;
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,5 +1,31 @@
+<script>
+    import { Accordion, AccordionItem, Collapse, Button, Card } from 'sveltestrap';
+    let isOpen = false;
+</script>
+
 <div class="container">
 
 <h1 class="mt-3">Home</h1>
+
+  <Accordion>
+    <AccordionItem active header="Home">Fallbrook</AccordionItem>
+    <AccordionItem header="School">
+      <a href="#home">Buena Vista Elementary</a>
+    </AccordionItem>
+    <AccordionItem header="Library">UCSB Library</AccordionItem>
+  </Accordion>
+
+  <hr>
+
+  <Button color="primary" on:click={() => (isOpen = !isOpen)} class="mb-3">
+    Toggle
+  </Button>
+  <Collapse {isOpen}>
+    <Card body>
+      Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry
+      richardson ad squid. Nihil anim keffiyeh helvetica, craft beer labore wes
+      anderson cred nesciunt sapiente ea proident.
+    </Card>
+  </Collapse>
 
 </div>

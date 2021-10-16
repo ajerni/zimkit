fca4befbd6b3e7e7a8894d64a73d0b7edf17a43a
ajerni <ajerni@gmail.com>
2021-10-16 12:15:29 +0200

    autorotate disabled

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -135,6 +135,7 @@
 <svelte:head>
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1" />
+	<meta http-equiv="ScreenOrientation" content="autoRotate:disabled">
 
 	<link
 		rel="stylesheet"
